c445f820bf80bd5e8bc76ef210e56c903dcde24f
pjaithong <pjaithong@indeed.com>
2024-07-08 06:22:58 +0900

    Updates

---
+++ precache-manifest.206d978dba877746e0f2410203248783.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ad99c3199a9878517d5b121a1bac09d",
+    "url": "/recipe-crud/index.html"
+  },
+  {
+    "revision": "25aba19a73f209d2caa7",
+    "url": "/recipe-crud/static/css/main.64502a0e.chunk.css"
+  },
+  {
+    "revision": "d5b4d0fb8be146f2f199",
+    "url": "/recipe-crud/static/js/2.07acb89f.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/recipe-crud/static/js/2.07acb89f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25aba19a73f209d2caa7",
+    "url": "/recipe-crud/static/js/main.9d644183.chunk.js"
+  },
+  {
+    "revision": "1f7ce0cb7f02863dba83",
+    "url": "/recipe-crud/static/js/runtime-main.b169a7bb.js"
+  }
+]);
